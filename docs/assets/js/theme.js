7c6d2d47fc422a487373a52dc4cf9cd0a94d7fe7
Kitsune Solar <kitsune.solar@gmail.com>
2019-04-01 14:43:12 +0300

    2019-04-01 11:43:12

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -9,7 +9,7 @@ function extJS_initPlayer() {
 	let elRadio = $('#radio');
 	//let elSource = $('#radio source').attr('src');
 	let count = Math.floor((Math.random() * 1000000) + 1);
-	let source = 'http://213.248.20.102:8881';
+	let source = 'http://213.248.20.102:8881/';
 
 	elRadio.mediaelementplayer({
 		success: function (media, node) {
@@ -17,7 +17,6 @@ function extJS_initPlayer() {
 			let button = target.find('.mejs__playpause-button');
 
 			button.on('click', function (e) {
-
 				if (media.readyState > 0) {
 					media.pause();
 					media.setSrc('');
@@ -30,7 +29,6 @@ function extJS_initPlayer() {
 					media.play();
 					button.removeClass('mejs__pause').addClass('mejs__play');
 				}
-
 			});
 		}
 	});
