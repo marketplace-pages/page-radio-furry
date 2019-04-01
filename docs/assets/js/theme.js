7e4eca02cc2a232742ba07f2ee302e06b1df3671
Kitsune Solar <kitsune.solar@gmail.com>
2019-04-01 13:28:11 +0300

    2019-04-01 10:28:11

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -7,9 +7,15 @@
 
 function extJS_initPlayer() {
 	let elPlayer = $('#player');
+	let elSource = $('#player source').attr('src');
 	elPlayer.mediaelementplayer({
 		success: function (player, node) {
-			$(player).closest('.mejs__container').attr('lang', mejs.i18n.language());
+			$(player).closest('.mejs__container');
+			let count = Math.floor( ( Math.random() * 1000000 ) + 1 );
+			let source = $('#player source');
+			$(player).setSrc( elSource + '?nocache=' + i );
+
+			alert(elSource);
 		}
 	});
 }
