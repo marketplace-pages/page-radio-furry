f03583d1fc7fa6a0526d9e632492c6026a91bf57
Kitsune Solar <kitsune.solar@gmail.com>
2019-04-01 13:20:24 +0300

    2019-04-01 10:20:24

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -5,33 +5,14 @@
  * ---------------------------------------------------------------------------------------------------------------------
  */
 
-$(window).load(function () {
-
-	let target = $('.player-container').closest('.mejs__container'),
-		playerId = target.attr('id'),
-		player = mejs.players[playerId],
-		button = target.find('.mejs__playpause-button'),
-		i = Math.floor((Math.random() * 1000000) + 1),
-		source = 'http://213.248.20.102:8881/;';
-
-	button.on('click', function () {
-
-		if (player.media.readyState > 0) {
-			player.pause();
-			player.setSrc('');
-			player.load();
-			button.removeClass('mejs__pause').addClass('mejs__play');
-		} else {
-			i++;
-			player.setSrc(source + '?nocache=' + i);
-			player.load();
-			player.play();
-			button.removeClass('mejs__play').addClass('mejs__pause');
+function extJS_initPlayer() {
+	let elPlayer = $('#player');
+	elPlayer.mediaelementplayer({
+		success: function (player, node) {
+			$(player).closest('.mejs__container').attr('lang', mejs.i18n.language());
 		}
-
-		return false;
 	});
-});
+}
 
 /**
  * Loading functions.
